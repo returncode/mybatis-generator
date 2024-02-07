55cc987b25fca0b4609147d08415003220702475
wanglei <wanglei@ctdcn.com>
2024-02-07 10:25:56 +0800

    update code

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # mybatis-generator
 mybatis-generator 代码生成工具
 
-1、修改[code.properties](src%2Fmain%2Fresources%2Fcode.properties)下基本参数
+1、修改[CodeGeneratorConfig.json](src%2Fmain%2Fresources%2FCodeGeneratorConfig.json)下代码生成参数。
 
-2、执行CodeGenerator中的main方法，按控制台提示输入，即可生成对应代码。  
+2、执行CodeGenerator中的main方法，即可生成对应代码。  
 
 3、如果数据库表没有前缀，随便输入一个字符代替即可。
--- src/main/java/com/returncode/tools/CodeGenerator.java
+++ src/main/java/com/returncode/tools/CodeGenerator.java
@@ -1,6 +1,8 @@
 package com.returncode.tools;
 
-import cn.hutool.setting.dialect.Props;
+import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.generator.FastAutoGenerator;
 import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
@@ -13,8 +15,6 @@ import java.util.Collections;
  * https://baomidou.com/pages/981406/
  */
 public class CodeGenerator {
-    /** 项目路径 */
-    private static final String PROJECT = System.getProperty("user.dir");
     /** 数据库连接 */
     private static String url;
     /** 数据库用户 */
@@ -24,7 +24,7 @@ public class CodeGenerator {
     /** 作者 */
     private static String author;
     /** 输出目录 */
-    private static String outputDir;
+    private static String codePath;
     /** 包命名 */
     private static String packagePath;
     /** 模块目录 */
@@ -32,7 +32,7 @@ public class CodeGenerator {
     /** 实体目录 */
     private static String entity;
     /** XML目录 */
-    private static String mapperXml;
+    private static String xmlPath;
     /** 转换表名 */
     private static String[] tables;
     /** 去除表前缀 */
@@ -41,81 +41,95 @@ public class CodeGenerator {
     public static void main(String[] args) {
         initCodeGeneratorConfig();
         FastAutoGenerator.create(new DataSourceConfig.Builder(url, username, password))
-                // 全局配置
-                .globalConfig(builder -> { builder
-                    // 设置作者
-                    .author(author)
-                    //禁止打开输出目录
-                    .disableOpenDir()
-                    // 指定输出目录
-                    .outputDir(outputDir);
-                })
-                // 包配置
-                .packageConfig(builder -> { builder
-                    // 设置父包名
-                    .parent(packagePath)
-                    // 设置父包模块名
-                    .moduleName(module)
-                    //设置entity包名
-                    .entity(entity)
-                    // 设置mapperXml生成路径
-                    .pathInfo(Collections.singletonMap(OutputFile.xml, mapperXml));
-                })
+            // 全局配置
+            .globalConfig(builder -> { builder
+                // 设置作者
+                .author(author)
+                //禁止打开输出目录
+                .disableOpenDir()
+                // 指定输出目录
+                .outputDir(codePath);
+            })
+            // 包配置
+            .packageConfig(builder -> { builder
+                // 设置父包名
+                .parent(packagePath)
+                // 设置父包模块名
+                .moduleName(module)
+                //设置entity包名
+                .entity(entity)
+                // 设置mapperXml生成路径
+                .pathInfo(Collections.singletonMap(OutputFile.xml, xmlPath));
+            })
+            // 注入配置
+            .injectionConfig(builder -> { builder
                 // 注入配置
-                .injectionConfig(builder -> {builder
-                    // 注入配置
-                    .beforeOutputFile((tableInfo, objectMap) -> {
-                        tableInfo.getImportPackages().remove(TableName.class.getCanonicalName());
-                        tableInfo.setConvert(false);
-                    });
-                })
-                // 策略配置
-                .strategyConfig(builder -> {
-                    builder.enableSchema()
-                            .addInclude(tables)
-                            .addTablePrefix(tablePrefix);
-                    builder.entityBuilder()
-                            .formatFileName("%sPO")
-                            .enableFileOverride();
-                    builder.controllerBuilder()
-                            .enableRestStyle()
-                            .enableFileOverride();
-                    builder.serviceBuilder()
-                            .superServiceClass("")
-                            .superServiceImplClass("")
-                            .enableFileOverride();
-                    builder.mapperBuilder()
-                            .superClass("")
-                            .enableFileOverride()
-                            .enableBaseResultMap()
-                            .enableBaseColumnList();
-                }).execute();
+                .beforeOutputFile((tableInfo, objectMap) -> {
+                    tableInfo.getImportPackages().remove(TableName.class.getCanonicalName());
+                    tableInfo.setConvert(false);
+                });
+            })
+            // 策略配置
+            .strategyConfig(builder -> {
+                builder.enableSchema()
+                        .addInclude(tables)
+                        .addTablePrefix(tablePrefix);
+                builder.entityBuilder()
+                        .formatFileName("%sPO")
+                        .enableFileOverride();
+                builder.controllerBuilder()
+                        .enableRestStyle()
+                        .enableFileOverride();
+                builder.serviceBuilder()
+                        .superServiceClass("")
+                        .superServiceImplClass("")
+                        .enableFileOverride();
+                builder.mapperBuilder()
+                        .superClass("")
+                        .enableFileOverride()
+                        .enableBaseResultMap()
+                        .enableBaseColumnList();
+            }).execute();
     }
 
     /** 初始化代码生成设置 */
     private static void initCodeGeneratorConfig() {
-        Props props = new Props("code.properties");
+        String propsJson = ResourceUtil.readUtf8Str("CodeGeneratorConfig.json");
+        JSONObject props = JSONUtil.parseObj(propsJson);
+        // 数据库配置
+        JSONObject dataSource = props.getJSONObject("dataSource");
         // 数据库连接
-        url = props.getProperty("url");
+        url = dataSource.getStr("url");
         // 数据库用户
-        username = props.getProperty("username");
+        username = dataSource.getStr("username");
         // 数据库密码
-        password = props.getProperty("password");
+        password = dataSource.getStr("password");
+
+        // 全局配置
+        JSONObject globalConfig = props.getJSONObject("globalConfig");
         // 作者
-        author = props.getProperty("author");
+        author = globalConfig.getStr("author");
+        // 项目地址
+        String projectPath = globalConfig.getStr("projectPath");
         // 输出目录
-        outputDir = PROJECT + props.getProperty("outputDir");
-        // 包命名
-        packagePath = props.getProperty("packagePath");
+        codePath = projectPath + globalConfig.getStr("codePath");
+
+        // 包配置
+        JSONObject packageConfig = props.getJSONObject("packageConfig");
+        // 包命名codePath
+        packagePath = packageConfig.getStr("packagePath");
         // 模块目录
-        module = props.getProperty("module");
+        module = packageConfig.getStr("module");
         // 实体目录
-        entity = props.getProperty("entity");
+        entity = packageConfig.getStr("entity");
         // XML目录
-        mapperXml = PROJECT + props.getProperty("mapperXml");
+        xmlPath = projectPath + packageConfig.getStr("xmlPath");
+
+        // 策略配置
+        JSONObject strategyConfig = props.getJSONObject("strategyConfig");
         // 转换表名
-        tables = props.getProperty("tables").split(",");
+        tables = strategyConfig.getStr("tables").split(",");
         // 去除表前缀
-        tablePrefix = props.getProperty("tablePrefix");
+        tablePrefix = strategyConfig.getStr("tablePrefix");
     }
 }
